6d0c52a93bdeffe8e2e755491566f90043e83d6f
simoneaucello <simoneaucello@gmail.com>
2024-04-16 15:35:39 +0200

    [add] function password generator

--- index.php
+++ index.php
@@ -1,16 +1,36 @@
 <?php
 
-function generateRandomPassword()
+// Genero una password casuale con ALMENO una lettera minuscola, una lettera maiuscola, un numero e un simbolo
+function generateRandomPassword($length)
 {
   $lowercase_letters = 'abcdefghijklmnopqrstuvwxyz';
   $uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   $numbers = '0123456789';
   $symbols = '!?&%$<>^+-*/()[]{}@#_=';
 
-  $characters = $lowercase_letters . $uppercase_letters . $numbers . $symbols;
+  $all_characters = $lowercase_letters . $uppercase_letters . $numbers . $symbols;
 
   $password = '';
+  $password .= $lowercase_letters[rand(0, strlen($lowercase_letters) - 1)];
+  $password .= $uppercase_letters[rand(0, strlen($uppercase_letters) - 1)];
+  $password .= $numbers[rand(0, strlen($numbers) - 1)];
+  $password .= $symbols[rand(0, strlen($symbols) - 1)];
+
+  $remaining_length = $length - 4;
+  for ($i = 0; $i < $remaining_length; $i++) {
+    $password .= $all_characters[rand(0, strlen($all_characters) - 1)];
+  }
+
+  // Mescola i caratteri della password in modo casuale
+  $password = str_shuffle($password);
+
+  return $password;
 }
+
+var_dump(generateRandomPassword($_GET['password_length']));
+
+
+$password_length = $_GET['password_length'];
 
 include __DIR__ . '/partials/head.php'
 
@@ -30,8 +50,9 @@ include __DIR__ . '/partials/head.php'
 
         <div class=" d-flex justify-content-center">
           <button type="submit" class="btn btn-danger">GENERA PASSWORD</button>
-
         </div>
+
+
 
 
 
